74afae9e569e055898b9c350749b106098edc0b6
Svyatoslav Boytsov <sbo@tassta.com>
2022-06-16 00:15:56 +0300

    custom Allure template added

--- src/test/java/specs/BaseSpec.java
+++ src/test/java/specs/BaseSpec.java
@@ -1,17 +1,17 @@
 package specs;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
-import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-
-import static helpers.CustomApiListener.withCustomTemplates;
 
 public class BaseSpec {
 
     public static RequestSpecification requestSpecification = new RequestSpecBuilder()
-            .addFilter(new RequestLoggingFilter())
+            .addFilter(new AllureRestAssured()
+                    .setRequestTemplate("request.ftl")
+                    .setResponseTemplate("response.ftl"))
             .setContentType(ContentType.JSON)
             .log(LogDetail.ALL)
             .build();
